cf4de86f9709904d2ce7ebce43e2a9a98cb22bd5
xyyx <xyyx@mail.ru>
2016-01-19 12:35:57 +0800

    Add skip hdcp option
    needs for 2f13f80

--- device.mk
+++ device.mk
@@ -146,6 +146,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	persist.audio.fluence.mode=endfire \
 	persist.audio.lowlatency.rec=false
 
+# Skip HDCP
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.wfd.nohdcp=1
 
 # Do not power down SIM card when modem is sent to Low Power Mode.
 PRODUCT_PROPERTY_OVERRIDES += \
